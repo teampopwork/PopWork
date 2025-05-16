5494ef4cb27fbc26837be1a51e5eb0bc2758e6c0
YourLocalMoon <yourlocalmoon7@gmail.com>
2025-05-16 18:41:09 +0300

    idk

--- tools/PopPak/poppak.cpp
+++ tools/PopPak/poppak.cpp
@@ -165,10 +165,9 @@ int main(int argc, char* argv[])
 {
 	if (argc <= 1)
 	{
-		std::cerr << "Usage: PopPak [/U] [/P] [/K \"The Password in Quotes\"] <FileName> <DirPath>" << std::endl;
+		std::cerr << "Usage: PopPak [/U] [/P] <FileName> <DirPath>" << std::endl;
 		std::cerr << "  /U    Unpacks pak file to DirPath" << std::endl;
 		std::cerr << "  /P    Creates pak file from files in DirPath" << std::endl;
-		std::cerr << "  /K    Changes the Default Encryption Password" << std::endl;
 		return 1;
 	}
 
@@ -182,17 +181,6 @@ int main(int argc, char* argv[])
 				unpackage = true;
 			else if (stricmp(argv[anArgPos], "/P") == 0)
 				package = true;
-			else if (stricmp(argv[anArgPos], "/K") == 0)
-			{
-				anArgPos++;
-				gEncryptPassword = argv[anArgPos];
-
-				if(gEncryptPassword == "")
-				{
-					std::cerr << "Invalid Password:" << argv[anArgPos] << std::endl;
-					return 106;
-				}
-			}
 			else
 			{
 				std::cerr << "Invalid option" << std::endl;
@@ -206,28 +194,24 @@ int main(int argc, char* argv[])
 	}
 
 	////////////////////////////////////////////////////////////////////////////
-	// PopCapPWE should be an internal tool.  In any event, the default Password
+	// PopPak should be an internal tool.  In any event, the default Password
 	// is actually really easy to guess.
-	std::cout << StrFormat("Password: %s", gEncryptPassword.c_str()).c_str() << std::endl;
-	if (gEncryptPassword == "PopCapPopWorkFramework")
-		std::cerr << StrFormat("WARNING: %s is the Default Password and is NOT allowed for Distribution!", gEncryptPassword.c_str()).c_str() << std::endl;
+	///std::cout << StrFormat("Password: %s", gEncryptPassword.c_str()).c_str() << std::endl;
 
 	if (argc < anArgPos + 2)
 	{
-		std::cerr << "Usage: PopPak [/U] [/P] [/K \"The Password in Quotes\"] <FileName> <DirPath>" << std::endl;
+		std::cerr << "Usage: PopPak [/U] [/P] <FileName> <DirPath>" << std::endl;
 		std::cerr << "  /U    Unpacks pak file to DirPath" << std::endl;
 		std::cerr << "  /P    Creates pak file from files in DirPath" << std::endl;
-		std::cerr << "  /K    Changes the Default Encryption Password" << std::endl;
 		return 102;
 	}
 
 	if (package && unpackage)
 	{
 		std::cerr << "/U and /P may not be specified together." << std::endl;
-		std::cerr << "Usage: PopPak [/U] [/P] [/K \"The Password in Quotes\"] <FileName> <DirPath>" << std::endl;
+		std::cerr << "Usage: PopPak [/U] [/P] <FileName> <DirPath>" << std::endl;
 		std::cerr << "  /U    Unpacks pak file to DirPath" << std::endl;
 		std::cerr << "  /P    Creates pak file from files in DirPath" << std::endl;
-		std::cerr << "  /K    Changes the Default Encryption Password" << std::endl;
 		return 103;
 	}
 
