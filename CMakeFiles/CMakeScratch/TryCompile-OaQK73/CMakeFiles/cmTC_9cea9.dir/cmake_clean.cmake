dcc64fbafa3092f3fd361c417f169359b8a4a28b
UNKNOWN <>
2025-06-10 21:06:18 +0400

    setup script

---
+++ CMakeFiles/CMakeScratch/TryCompile-OaQK73/CMakeFiles/cmTC_9cea9.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_9cea9.dir/CMakeCXXCompilerABI.cpp.o"
+  "cmTC_9cea9"
+  "cmTC_9cea9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_9cea9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/CMakeScratch/TryCompile-OaQK73/CMakeFiles/cmTC_9cea9.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_9cea9.
+# This may be replaced when dependencies are built.
